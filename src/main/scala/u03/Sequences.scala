8b9782a4f8e486cb350df468c7a6531cd8bfac74
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-03-18 12:05:15 +0100

    Complete first execise

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -19,18 +19,40 @@ object Sequences: // Essentially, generic linkedlists
       case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
       case Nil()      => Nil()
 
+    def mapWithFlatMap[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = 
+      flatMap(l)(a => Cons(mapper(a), Nil()))
+
     def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = l1 match
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t)            => filter(t)(pred)
       case Nil()                 => Nil()
 
-    // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def filterwithFlatmap[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = 
+      val m = (v: A) => v match
+        case v if pred(v) => Cons(v, Nil())
+        case _ => Nil()
+      flatMap(l1)(m)
+      
 
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
+    // Lab 03
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(f, s), Cons(f2, s2)) => Cons((f, f2), zip(s, s2))
+      case _ => Nil()
     
-    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
-    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
+
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
+      case Cons(h, t) if n > 0 =>  Cons(h, take(t)(n - 1))
+      case _ => Nil()
+    
+    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = (l1, l2) match
+      case (Cons(h, t), l2) => Cons(h, concat(t, l2))
+      case (Nil(), Cons(h, t)) => Cons(h, concat(t, Nil()))
+      case _ => Nil()
+    
+    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = l match
+      case Nil() => Nil()
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper)) 
+    
 
     def min(l: Sequence[Int]): Optional[Int] = ???
     
